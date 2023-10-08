834437a705498a84cf42cb4238293b82df416b47
Jatin Foujdar <jatin321001@gmail.com>
2023-10-08 21:47:05 +0530

    enviroment setup commit

---
+++ src/config/config.js
@@ -0,0 +1,13 @@
+const config ={
+
+    appwriteUrl:String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId:(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId:(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId:(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId:(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+
+}
+
+
+
+export default config
